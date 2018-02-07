1a590da78764a991ed90876f339c6bc6999792c2
Steve Shaffer <SteveShaffer@users.noreply.github.com>
2018-02-06 23:34:21 -0800

    oops

--- src/modules/mediaExample/index.js
+++ src/modules/mediaExample/index.js
@@ -196,7 +196,7 @@ export function transcribeMedia(file) {
           targetId: ${recordingId},
           tasks :[
             {
-              engineId: "fc004413-89f0-132a-60b2-b94522fb7e66" // Cerebral (Transcoder)
+              engineId: "fc004413-89f0-132a-60b2-b94522fb7e66" # Cerebral (Transcoder)
             },
             {
               engineId: "${engineId}"
